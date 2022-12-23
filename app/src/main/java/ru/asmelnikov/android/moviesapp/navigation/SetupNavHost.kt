82a2086c82f04051797d1810f5d5d6306f8b6b87
Your Name <melnikov150990@mail.ru>
2022-12-23 21:47:56 +0500

    first day: navigation, main screen, splash screen, animation

---
+++ app/src/main/java/ru/asmelnikov/android/moviesapp/navigation/SetupNavHost.kt
@@ -0,0 +1,35 @@
+package ru.asmelnikov.android.moviesapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import ru.asmelnikov.android.moviesapp.screens.MainScreen
+import ru.asmelnikov.android.moviesapp.screens.SplashScreen
+import ru.asmelnikov.android.moviesapp.utils.Constants.Screens.DETAILS_SCREEN
+import ru.asmelnikov.android.moviesapp.utils.Constants.Screens.MAIN_SCREEN
+import ru.asmelnikov.android.moviesapp.utils.Constants.Screens.SPLASH_SCREEN
+
+sealed class Screens(val route: String) {
+    object Splash : Screens(route = SPLASH_SCREEN)
+    object Main : Screens(route = MAIN_SCREEN)
+    object Details : Screens(route = DETAILS_SCREEN)
+}
+
+@Composable
+fun SetupNavHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screens.Splash.route
+    ) {
+        composable(route = Screens.Splash.route) {
+            SplashScreen(navController = navController)
+        }
+        composable(route = Screens.Main.route) {
+            MainScreen()
+        }
+        composable(route = Screens.Details.route) {
+
+        }
+    }
+}
